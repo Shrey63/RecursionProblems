b73f9af32de6a5374dbe5e6b6437b1e1146b179b
Shrey63 <shreyaparasagrawal@gmail.com>
2023-07-05 07:25:37 +0530

    Sum of digits done

---
+++ src/SumOfDigits.java
@@ -0,0 +1,12 @@
+public class SumOfDigits {
+    public static void main(String[] args) {
+    int num=345;
+        System.out.println(sod(345));
+    }
+    public static int sod(int num)
+    {
+        if(num==0)
+            return 0;
+        return num%10+sod(num/10);
+    }
+}
